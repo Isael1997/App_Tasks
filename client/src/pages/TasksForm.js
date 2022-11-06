613a0d6a2cd7e4b9aa8a1c99b4945a797a99d7d1
Ronald <ronald.isael@hotmail.com>
2022-11-06 01:29:04 -0400

    Add tasks button is modified

--- client/src/pages/TasksForm.js
+++ client/src/pages/TasksForm.js
@@ -6,35 +6,49 @@ function TasksForm() {
         <div>
             <Formik
                 initialValues={{ title: "", description: "" }}
-                onSubmit={async (values) => {
+                onSubmit={async (values, actions) => {
                     console.log(values);
                     try {
                         const response = await CreateTaskRequest(values);
                         console.log(response);
+
+                        actions.resetForm({
+                            values: {
+                                // the type of `values` inferred to be Blog
+                                title: '',
+                                description: '',
+                            },
+                            // you can also set the other form states here
+                        });
                     } catch (error) {
                         console.log(error);
                     }
                 }}
             >
-                <Form>
-                    {/*Title */}
-                    <label>title</label>
-                    <Field
-                        name="title"
-                        type="text"
-                        placeholder="Write a title"
-                    />
+                {(actions) => (
+                    <Form>
+                        {/*Title */}
+                        <label>title</label>
+                        <Field
+                            name="title"
+                            type="text"
+                            placeholder="Write a title"
+                        />
 
-                    {/**Description */}
-                    <label>description</label>
-                    <Field
-                        name="description"
-                        as="textarea"
-                        ows="3"
-                        placeholder="Write a description"
-                    />
-                    <button type="submit">Submit</button>
-                </Form>
+                        {/**Description */}
+                        <label>description</label>
+                        <Field
+                            name="description"
+                            as="textarea"
+                            ows="3"
+                            placeholder="Write a description"
+                        />
+                        <button type="submit" disabled={actions.isSubmitting}>
+                            {actions.isSubmitting ? "Saving..." : "Save"}
+                            </button>
+                    </Form>
+                )}
+
             </Formik>
         </div>
     )
